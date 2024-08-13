874b7594b9ec59aa717f46f58613a674ba38aa74
UycGitHub <umut.yusuf.cinar.mail@gmail.com>
2024-08-13 08:35:23 +0300

    Added: screens are edited. But this edit is not permanently.

--- app/src/main/java/com/umutyusufcinar/netflixclone/MainActivity.kt
+++ app/src/main/java/com/umutyusufcinar/netflixclone/MainActivity.kt
@@ -5,16 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.umutyusufcinar.netflixclone.di.MainComponent
+import com.umutyusufcinar.netflixclone.di.MoviesApplication
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainComponent: MainComponent
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        mainComponent = (application as MoviesApplication).appComponent.mainComponent().create()
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
