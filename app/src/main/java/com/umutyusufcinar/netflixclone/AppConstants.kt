eabfdf11ed4b362f46729198546dba1a67b78290
UycGitHub <umut.yusuf.cinar.mail@gmail.com>
2024-08-20 14:43:19 +0300

    Changed: The codes related to response attempts in the HomeViewModel.kt file have been updated. A showErrorScreen function has been written for the content of the getListOfMovies() function, which will catch the necessary errors and print an error message. The content of the error messages has also been added to the content of the AppConstants.kt file. In this way, action has been taken according to any response from homeDataSource.

--- app/src/main/java/com/umutyusufcinar/netflixclone/AppConstants.kt
+++ app/src/main/java/com/umutyusufcinar/netflixclone/AppConstants.kt
@@ -5,4 +5,10 @@ object AppConstants {
     const val TMDB_API_KEY ="ba4ff6c34e362f047c5418b959e9841e" //change later, changed
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val LANGUAGE = "en-US"
+
+    //    ERROR MESSAGES
+    const val API_ERROR_MESSAGE = "Resource not found :/\n \"Please, try again later\""
+    const val NETWORK_ERROR_MESSAGE = "Network Error :/\n \"Please, check your connection and try again\""
+    const val UNKNOWN_ERROR_MESSAGE = "Unexpected Error :/\\n \"Please, check your connection and try again\""
+    const val SEARCH_NO_RESULtS = "No results."
 }
--- app/src/main/java/com/umutyusufcinar/netflixclone/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/umutyusufcinar/netflixclone/viewmodel/HomeViewModel.kt
@@ -1,37 +1,86 @@
 package com.umutyusufcinar.netflixclone.viewmodel
 
+import android.content.Context
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.umutyusufcinar.netflixclone.AppConstants
+import com.umutyusufcinar.netflixclone.di.IoDispatcher
 import com.umutyusufcinar.netflixclone.network.NetworkResponse
 import com.umutyusufcinar.netflixclone.network.TmdbApi
+import com.umutyusufcinar.netflixclone.network.model.dto.MovieDTO
 import com.umutyusufcinar.netflixclone.network.model.dto.MovieResponseDTO
+import com.umutyusufcinar.netflixclone.repository.HomeDataSource
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import javax.inject.Inject
+//responses are tried before
+class HomeViewModel @Inject constructor(val context: Context, private val tmdbApi: TmdbApi, private val homeDataSource: HomeDataSource, @IoDispatcher private val ioDispatcher: CoroutineDispatcher) : ViewModel() {
 
-class HomeViewModel @Inject constructor(val tmdbApi: TmdbApi): ViewModel() {
-    fun getTrending(){
-        viewModelScope.launch {
-            val response = tmdbApi.getTrending("en-US", page = 1)
-            when(response){
-                is NetworkResponse.Success -> {
-                    Log.d("HomeViewModel", "Success")
+    private val _listsOfMovies: MutableLiveData<List<List<MovieDTO>>?> = MutableLiveData()
+    val listsOfMovies: LiveData<List<List<MovieDTO>>?> = _listsOfMovies
 
-            }
-            is NetworkResponse.ApiError -> {
-                Log.d("HomeViewModel", "ApiError")
-            }
-            is NetworkResponse.NetworkError -> {
-                Log.d("HomeViewModel", "NetworkError")
-            }
 
-            is NetworkResponse.UnknownError -> {
-                Log.d("HomeViewModel", "UnknownError")
+
+    private val _isLoading: MutableLiveData<Boolean> = MutableLiveData()
+    val isLoading: MutableLiveData<Boolean> = _isLoading
+
+    private var _errorScreenVisibility = MutableLiveData<Boolean>(false)
+    var errorScreenVisibility: LiveData<Boolean> = _errorScreenVisibility
+
+    private var _errorMessage = MutableLiveData<String?>()
+    var errorMessage: MutableLiveData<String?> = _errorMessage
+
+    init {
+        getListOfMovies()
+    }
+
+    fun getListOfMovies() {
+        showErrorScreen(false)
+        try {
+            viewModelScope.launch(ioDispatcher) {
+
+                homeDataSource.getListOfMovies(ioDispatcher) { result ->
+                    when (result) {
+                        is NetworkResponse.Success -> {
+                            _listsOfMovies.postValue(result.body)
+                        }
+                        is NetworkResponse.ApiError -> {
+                            showErrorScreen(show = true, message = AppConstants.API_ERROR_MESSAGE)
+                        }
+                        is NetworkResponse.NetworkError -> {
+                            showErrorScreen(
+                                show = true,
+                                message = AppConstants.NETWORK_ERROR_MESSAGE
+                            )
+                        }
+                        is NetworkResponse.UnknownError -> {
+                            showErrorScreen(
+                                show = true,
+                                message = AppConstants.UNKNOWN_ERROR_MESSAGE
+                            )
+                        }
+                    }
+                }
             }
+        } catch (exception: Exception){
+            throw exception
         }
     }
+
+
+
+    private fun showErrorScreen(show: Boolean, message: String? = null) {
+        _errorMessage.postValue(message)
+        _errorScreenVisibility.postValue(show)
+        _isLoading.postValue(!show)
     }
+
+
+
 }
