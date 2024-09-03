6efcde035663546a3130e7b03aba3dad21818137
UycGitHub <umut.yusuf.cinar.mail@gmail.com>
2024-09-03 15:16:47 +0300

    Updated: new TMDB_BASE_URL and TMDB_IMAGE_BASE_URL's added to AppConstants.kt file.

--- app/src/main/java/com/umutyusufcinar/netflixclone/AppConstants.kt
+++ app/src/main/java/com/umutyusufcinar/netflixclone/AppConstants.kt
@@ -4,6 +4,14 @@ object AppConstants {
     //ba4ff6c34e362f047c5418b959e9841e
     const val TMDB_API_KEY ="ba4ff6c34e362f047c5418b959e9841e" //change later, changed
     const val BASE_URL = "https://api.themoviedb.org/3/"
+
+    const val TMDB_BASE_URL = "https://api.themoviedb.org/3/"
+    const val TMDB_IMAGE_BASE_URL_W185 = "https://image.tmdb.org/t/p/w185/"
+    const val TMDB_IMAGE_BASE_URL_W500 = "https://image.tmdb.org/t/p/w500/"
+    const val TMDB_IMAGE_BASE_URL_W780 = "https://image.tmdb.org/t/p/w780/"
+    const val TMDB_IMAGE_BASE_URL_ORIGINAL = "https://image.tmdb.org/t/p/original/"
+    const val TMDB_IMAGE_BASE_URL_W1280 = "https://image.tmdb.org/t/p/w1280/"
+
     const val LANGUAGE = "en-US"
 
     //    ERROR MESSAGES
