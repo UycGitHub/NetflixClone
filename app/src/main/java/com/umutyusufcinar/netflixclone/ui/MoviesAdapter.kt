cc9cbf3f2991e6568d6c7601cdab9a0e6b18250a
UycGitHub <umut.yusuf.cinar.mail@gmail.com>
2024-09-03 15:26:03 +0300

    Updated: MoviesAdapter.kt updated for Picasso using.

--- app/src/main/java/com/umutyusufcinar/netflixclone/ui/MoviesAdapter.kt
+++ app/src/main/java/com/umutyusufcinar/netflixclone/ui/MoviesAdapter.kt
@@ -1,16 +1,18 @@
 package com.umutyusufcinar.netflixclone.ui
 
-
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import com.umutyusufcinar.netflixclone.AppConstants
 import com.umutyusufcinar.netflixclone.R
 import com.umutyusufcinar.netflixclone.network.model.dto.MovieDTO
 
-class MovieAdapter (val context: Context, val movies: List<MovieDTO>)
+class MovieAdapter(val context: Context, val movies: List<MovieDTO>)
     : RecyclerView.Adapter<MovieAdapter.MovieHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieHolder {
@@ -21,11 +23,16 @@ class MovieAdapter (val context: Context, val movies: List<MovieDTO>)
     override fun getItemCount(): Int = movies.size
 
     override fun onBindViewHolder(holder: MovieHolder, position: Int) {
-        movies.elementAt(position).apply {  }
+        movies.elementAt(position).apply {
+            Picasso.get()
+                .load("${AppConstants.TMDB_IMAGE_BASE_URL_W185}${poster_path}")
+                .placeholder(R.drawable.placeholder)
+                .into(holder.poster)
+        }
     }
 
-    class MovieHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        private var cardView = itemView.cv_movie
-        private var poster = itemView.iv_movie_poster
+    class MovieHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var cardView: CardView = itemView.findViewById(R.id.cv_movie)
+        var poster: ImageView = itemView.findViewById(R.id.iv_movie_poster)
     }
 }
