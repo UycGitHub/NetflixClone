b152bbb164b216ea2e47ecf7b06e9cfe5b147486
UycGitHub <umut.yusuf.cinar.mail@gmail.com>
2024-09-03 15:35:26 +0300

    Updated: "private" keyword deleted from MovieListHolder class' variables. LayoutManager and MovieAdapter edited, so problems are fixed.

--- app/src/main/java/com/umutyusufcinar/netflixclone/ui/ListsOfMoviesAdapter.kt
+++ app/src/main/java/com/umutyusufcinar/netflixclone/ui/ListsOfMoviesAdapter.kt
@@ -4,12 +4,13 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.umutyusufcinar.netflixclone.R
 import com.umutyusufcinar.netflixclone.network.model.dto.MovieDTO
 
-class ListsOfMoviesAdapter (val context: Context, val lists: List<List<MovieDTO>>)
+class ListsOfMoviesAdapter(val context: Context, val lists: List<List<MovieDTO>>)
     : RecyclerView.Adapter<ListsOfMoviesAdapter.MovieListHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieListHolder {
@@ -20,18 +21,18 @@ class ListsOfMoviesAdapter (val context: Context, val lists: List<List<MovieDTO>
     override fun getItemCount(): Int = lists.size
 
     override fun onBindViewHolder(holder: MovieListHolder, position: Int) {
-        holder.title.text = when(position){
+        holder.title.text = when(position) {
             0 -> "Trending Now"
             1 -> "Upcoming Movies"
             2 -> "Popular Movies"
             else -> "Movies"
         }
-        holder.movies.LayoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        holder.movies.adapter = MoviesAdapter(context, lists[position])
+        holder.movies.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        holder.movies.adapter = MovieAdapter(context, lists[position])
     }
 
-    class MovieListHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        private var title = itemView.tv_movies_title
-        private var movies = itemView.rv_movies
+    class MovieListHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var title: TextView = itemView.findViewById(R.id.tv_movies_title)
+        var movies: RecyclerView = itemView.findViewById(R.id.rv_movies)
     }
 }
